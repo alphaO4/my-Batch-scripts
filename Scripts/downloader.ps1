d8438157ff28c8c7597482bd4783965070618f23
Jonah Svoboda <jonah.svoboda.2004@gmail.com>
2020-07-07 10:26:46 +0200

    corrected typo

--- Scripts/downloader.ps1
+++ Scripts/downloader.ps1
@@ -1,4 +1,4 @@
 $source = "Http link"; $destination = "File name. (doesn’t necessarily need the same name as the file getting downloaded)"; Invoke-WebRequest $source -OutFile $destination; 
 
 //example: $source = "https://www.dropbox.com/File.exe?dl=1"; $destination = "Test.exe"; Invoke-WebRequest $source -OutFile $destination; 
-//the "?dl=1" at the end means that if you go on that url the file will be automaticly dow loaded.
+//the "?dl=1" at the end means that if you go on that url the file will be automaticly downloaded.
