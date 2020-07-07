1eff300c4eb8a5977be3cdf9cf5680570ee94a69
Jonah Svoboda <jonah.svoboda.2004@gmail.com>
2020-07-07 23:40:59 +0200

    changed from json to powershell and added some code.
    I also added api keys so this will probably never be pushed, or if
    pushed the api keys will be removed.

--- Scripts/uploader.ps1
+++ Scripts/uploader.ps1
@@ -1,3 +1,9 @@
-//the following is a idea a got wile sitting in a cafe. at the end it should upload a file to anonfiles.com and echo the url to access it.
+#the following is a idea a got while sitting in a cafe. at the end it should upload a file to anonfiles.com and echo the url to access it.
 
-cu"file=@test.txt" "https://api.anonfiles.com/upload"
+$Token = ?token=6e578f0f3525bf07
+$datapath = "C:\temp\testforapi\test.txt"
+$uri = https://api.anonfiles.com/upload
+
+ Invoke-WebRequest -uri $uri -Method Put -Infile $datapath -Token $Token
+ 
+ print "$r.Content"
