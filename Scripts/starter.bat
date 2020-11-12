280031fef4c77c4cdfe3ce5d62c6318e335fe8f8
AlphaO4 <jonah.svoboda.2004@gmail.com>
2020-11-12 11:22:17 +0100

    deleted starter.bat since it won't really do anything

--- Scripts/starter.bat
+++
@@ -1,3 +0,0 @@
-start "virus.exe" -runAS
-rem NTS wont work since a file cant delete it self completly, it will leave a file with a few bytes named "starter" but without a file extension
-del "starter.exe" /F /Q
